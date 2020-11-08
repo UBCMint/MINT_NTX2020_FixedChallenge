eac7d4b0ff3ad475a7136a29c928485e607edf5a
alxshev <ashevchuk2001@gmail.com>
2020-11-07 23:29:28 -0800

    added SPI demo code

---
+++ MINT_EEGFirmware/SPI Test.X/delay.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   delay.h
+ * Author: alxshev
+ *
+ * Created on November 7, 2020, 11:12 PM
+ */
+
+#ifndef DELAY_H
+#define	DELAY_H
+
+void delay_us(unsigned int us);
+void delay_ms(int ms);
+#endif
