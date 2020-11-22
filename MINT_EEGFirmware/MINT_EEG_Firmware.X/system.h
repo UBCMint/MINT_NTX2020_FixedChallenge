db6042ec8d5269fda6334e7df31fff8d270330e4
KingRedfern <petzenred@gmail.com>
2020-11-22 14:42:22 -0500

    My code won't compile - I think my peripheral library is installed wrong? Can someone else compile to check? Added potential SCK fix

--- MINT_EEGFirmware/MINT_EEG_Firmware.X/system.h
+++ MINT_EEGFirmware/MINT_EEG_Firmware.X/system.h
@@ -2,10 +2,10 @@
 /* System Level #define Macros                                                */
 /******************************************************************************/
 
-#define MCU_SCI_INDEX 1b0000
-#define MCU_SDO_INDEX 0b1000
-#define MCU_SS_INDEX 0b0111
-#define SCK_INDEX 0b0000 
+#define MCU_SCI_INDEX 1b0000    // pin 51 
+#define MCU_SDO_INDEX 0b1000    // pin 52 
+#define MCU_SS_INDEX 0b0111     // pin 63
+#define SCK_INDEX 0b0000        // pin 50 
 
 /* Define system operating frequency */
 #define SYS_FREQ     SYS_FREQ 8000000 // 8Mhz
